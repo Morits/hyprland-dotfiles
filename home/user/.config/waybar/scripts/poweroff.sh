7f37e244f5fda886828a675d6a84df67e70f8ecb
Jostein Eriksen <github@morits.net>
2023-04-24 02:34:18 +0200

    Added poweroff to waybar

---
+++ home/user/.config/waybar/scripts/poweroff.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+case $(wofi -d -L 6 -l 3 -W 100 -x -100 -y 10 \
+    -D insensitive=true \
+    -D gtk_dark=true \
+    -D dynamic_lines=true << EOF | sed 's/^ *//'
+    Shutdown
+    Reboot
+    Log off
+    Sleep
+    Lock
+    Cancel
+EOF
+) in
+    "Shutdown")
+        systemctl poweroff
+        ;;
+    "Reboot")
+        systemctl reboot
+        ;;
+    "Sleep")
+        systemctl suspend
+        ;;
+    "Lock")
+        swaylock
+        ;;
+    "Log off")
+        hyprctl dispatch exit
+        ;;
+esac
